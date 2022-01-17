6115621efca91f0fbad01cf7c700ece1ac2f5e87
Curtis Hough <chough14@gmail.com>
2022-01-17 16:57:48 -0500

    update icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ import './Home.css';
 function Home() {
     return (
         <div className='home-nav-buttons'>
-            <div className="home-nav-button standings-button">
+            <div className="home-nav-button standings-button rounded">
                 <img src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/standingsIcon.png'></img>
             </div>
-            <div className="home-nav-button leaderboard-button">
+            <div className="home-nav-button leaderboard-button rounded">
                 <img src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/leaderboardIcon.png'></img>
             </div>
 
