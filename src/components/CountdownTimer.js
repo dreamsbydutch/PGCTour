cd3cb7db258393755d83d778ec7d7df9fad87368
Curtis Hough <chough14@gmail.com>
2022-01-17 23:49:43 -0500

    add countdown timer

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './CountdownTimer.css';
+
+
+export default function CountdownApp(props) {
+
+    const [secondsRemaining, setSecondsRemaining] = useState(props.count);
+    const [status, setStatus] = useState('Started');
+
+    useInterval(
+        () => {
+            if (secondsRemaining > 0) {
+                setSecondsRemaining(secondsRemaining - 1)
+            } else {
+                setStatus('Stopped')
+            }
+        },
+        status === 'Started' ? 1000 : null,
+        // passing null stops the interval
+    )
+
+    const secondsToDisplay = secondsRemaining % 60
+    const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60
+    const minutesToDisplay = minutesRemaining % 60
+    const hoursRemaining = (minutesRemaining - minutesToDisplay) / 60
+    const hoursToDisplay = hoursRemaining % 24
+    const daysToDisplay = (hoursRemaining - hoursToDisplay) / 24
+
+    return (
+        <div className="countdown-timer-container">
+            <h1>Countdown until {props.tourneyName}</h1>
+            <div className="row-container">
+                <div className="tourney_logo">
+                    <img alt="Tourney Logo" src={props.tourneyLogo} />
+                </div>
+                <div className="countdown-timer-div" style={{ padding: 20 }}>
+                    {twoDigits(daysToDisplay)}:{twoDigits(hoursToDisplay)}:{twoDigits(minutesToDisplay)}:
+                    {twoDigits(secondsToDisplay)}
+                    <div className="mini-timer-labels">Days : Hours : Minutes : Seconds</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+// source: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+function useInterval(callback, delay) {
+    const savedCallback = useRef()
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current()
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay)
+            return () => clearInterval(id)
+        }
+    }, [delay])
+}
+
+// https://stackoverflow.com/a/2998874/1673761
+const twoDigits = (num) => String(num).padStart(2, '0')
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import useFetch from 'react-fetch-hook';
 
 // import Leaderboard from './Leaderboard';
+import CountdownTimer from './CountdownTimer';
 import './Home.css';
 
 function Home() {
-    return (
-        <div className='home-nav-buttons'>
-            <div className="home-nav-button standings-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/#/standings"><img alt='Standings Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/standingsIcon.png'></img></a>
-            </div>
-            <div className="home-nav-button leaderboard-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/#/leaderboard"><img alt='Leaderboard Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/leaderboardIcon.png'></img></a>
-            </div>
-            <div className="home-nav-button golferstats-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/#/golferstats"><img alt='Golfer Stats Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/golferstatsIcon.png'></img></a>
-            </div>
 
-        </div>
+
+    const { isLoading, error, data } = useFetch("https://opensheet.elk.sh/1ce6c0R9YggvfOxoilnZqqXfcAPxVKyaN0mly5_FOouk/3");
+    if (isLoading) return "Loading....";
+    if (error) return "Error!";
+
+    const filter_data = data.filter(obj => Number(obj.id) === Number('19'));
+    var start_date = new Date(filter_data[0].StartDate);
+    var current_date = new Date();
+    const INITIAL_COUNT = Math.round((start_date - current_date) / 1000);
+
+
+    return (
+        <>
+            <div className='home-nav-buttons'>
+                <div className="home-nav-button standings-button rounded">
+                    <a href="https://dreamsbydutch.github.io/pgctour/#/standings"><img alt='Standings Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/standingsIcon.png'></img></a>
+                </div>
+                <div className="home-nav-button leaderboard-button rounded">
+                    <a href="https://dreamsbydutch.github.io/pgctour/#/leaderboard"><img alt='Leaderboard Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/leaderboardIcon.png'></img></a>
+                </div>
+                <div className="home-nav-button golferstats-button rounded">
+                    <a href="https://dreamsbydutch.github.io/pgctour/#/golferstats"><img alt='Golfer Stats Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/golferstatsIcon.png'></img></a>
+                </div>
+            </div>
+            <div className="countdown-timer">
+                <CountdownTimer count={INITIAL_COUNT} tourneyName={filter_data[0].Tourney} tourneyLogo={filter_data[0].Logo} />
+            </div>
+        </>
     )
 }
 
