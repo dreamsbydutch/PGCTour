9f9307414a838129e432602a7a0def4906b94194
Curtis Hough <chough14@gmail.com>
2022-01-17 21:35:00 -0500

    trying to fix router issue 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,13 @@ function Home() {
     return (
         <div className='home-nav-buttons'>
             <div className="home-nav-button standings-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/standings"><img alt='Standings Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/standingsIcon.png'></img></a>
+                <a href="https://dreamsbydutch.github.io/pgctour/#/standings"><img alt='Standings Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/standingsIcon.png'></img></a>
             </div>
             <div className="home-nav-button leaderboard-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/leaderboard"><img alt='Leaderboard Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/leaderboardIcon.png'></img></a>
+                <a href="https://dreamsbydutch.github.io/pgctour/#/leaderboard"><img alt='Leaderboard Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/leaderboardIcon.png'></img></a>
             </div>
             <div className="home-nav-button golferstats-button rounded">
-                <a href="https://dreamsbydutch.github.io/pgctour/golferstats"><img alt='Golfer Stats Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/golferstatsIcon.png'></img></a>
+                <a href="https://dreamsbydutch.github.io/pgctour/#/golferstats"><img alt='Golfer Stats Icon' src='https://raw.githubusercontent.com/dreamsbydutch/pgctour/main/public/assets/golferstatsIcon.png'></img></a>
             </div>
 
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,33 +13,33 @@ function NavBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/pgctour">Home</Nav.Link>
-                        <Nav.Link href="/pgctour/leaderboard">Leaderboard</Nav.Link>
-                        <Nav.Link href="/pgctour/standings">Standings</Nav.Link>
-                        <Nav.Link href="/pgctour/golferstats">Golfer Stats</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour">Home</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour/#/leaderboard">Leaderboard</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour/#/standings">Standings</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour/#/golferstats">Golfer Stats</Nav.Link>
                         <NavDropdown title="Tournaments" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/pgctour/tournament/6">The Masters</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/9">PGA Championship</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/11">U.S. Open</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/14">The Open Championship</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/6">The Masters</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/9">PGA Championship</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/11">U.S. Open</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/14">The Open Championship</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/pgctour/tournament/2">Genesis Invitational</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/3">Arnold Palmer Invitational</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/4">The Players Championship</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/10">The Memorial Tournament</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/2">Genesis Invitational</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/3">Arnold Palmer Invitational</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/4">The Players Championship</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/10">The Memorial Tournament</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/pgctour/tournament/1">Waste Management Open</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/5">Valero Texas Open</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/7">Zurich Classic</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/8">Wells Fargo Championship</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/12">Travelers Championship</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/13">Genesis Scottish Open</NavDropdown.Item>
-                            <NavDropdown.Item href="/pgctour/tournament/15">Rocket Mortgage Classic</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/1">Waste Management Open</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/5">Valero Texas Open</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/7">Zurich Classic</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/8">Wells Fargo Championship</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/12">Travelers Championship</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/13">Genesis Scottish Open</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/15">Rocket Mortgage Classic</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/pgctour/tournament/16">Playoffs</NavDropdown.Item>
+                            <NavDropdown.Item href="https://dreamsbydutch.github.io/pgctour/#/tournament/16">Playoffs</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/pgctour/rulebook">Rulebook</Nav.Link>
-                        <Nav.Link href="/pgctour/history">Tour History</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour/#/rulebook">Rulebook</Nav.Link>
+                        <Nav.Link href="https://dreamsbydutch.github.io/pgctour/#/history">Tour History</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
