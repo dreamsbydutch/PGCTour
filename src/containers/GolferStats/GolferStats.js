464eb1f28df7273b1301d49f86d847671544ce3e
Curtis Hough <chough14@gmail.com>
2022-01-25 16:57:05 -0500

    create base golferstats page

--- src/containers/GolferStats/GolferStats.js
+++ src/containers/GolferStats/GolferStats.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import GolferStatsComponent from '../../components/GolferStats/GolferStatsComponent'
 
 function GolferStats() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [statsData, setStatsData] = useState();
+
+    useEffect(() => {
+        axios
+            .get('https://opensheet.elk.sh/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/7')
+            .then((res) => {
+                setStatsData(res.data)
+                setIsLoading(false)
+            })
+    }, [])
+    if (isLoading) return <LoadingSpinner />;
+
     return (
-        <div>
-            Stats
-        </div>
+        <GolferStatsComponent info={statsData} />
     )
 }
 
