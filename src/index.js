e01fa39223258bb0f487775132444efb4941ef7b
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2022-03-28 11:08:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
