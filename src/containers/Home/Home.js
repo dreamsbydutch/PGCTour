656801ccc9f7c95aea302923b4a57db02d5cbb40
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2022-06-01 12:20:46 -0400

    Update Home.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -9,6 +9,7 @@ import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import ChampAlert from '../../components/ChampAlert/ChampAlert'
 import SignUpButton from '../../components/SignUpButton/SignUpButton'
+import SeasonSignUpButton from '../../components/SignUpButton/SeasonSignUpButton'
 import StudioVideoDisplay from '../../components/StudioVideoDisplay/StudioVideoDisplay';
 
 function Home() {
@@ -44,6 +45,7 @@ function Home() {
                 <div className="homescreen-standings">
                     <Standings limit={10} />
                 </div></a>
+            <SeasonSignUpButton data="https://forms.gle/K9aRCRG56V384wpAA" />
         </>
     )
 }
