6389d1c4d77d826e1cf27e9cdeed4b7bebada65b
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2022-06-01 12:18:54 -0400

    Update SeasonSignUpButton.js

--- src/components/SignUpButton/SeasonSignUpButton.js
+++ src/components/SignUpButton/SeasonSignUpButton.js
@@ -4,7 +4,7 @@ import './SeasonSignUpButton.css'
 function SeasonSignUpButton(props) {
     return (
         <div className="seasonsignupbutton-container">
-            <a href={props.data.FormLink} className="seasonsignupbutton">SIGN UP FOR THE 2023 PGC TOUR HERE</a>
+            <a href={props.data} className="seasonsignupbutton">SIGN UP FOR THE 2023 PGC TOUR HERE</a>
         </div>
     )
 }
